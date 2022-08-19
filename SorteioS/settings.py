0697f4c142931ddd445f0fbaa6898b5989cfea7e
Rafael Lázaro <95374934+Odalag@users.noreply.github.com>
2022-08-19 11:35:25 -0300

    att do CA
    O servidor está rodando normalmente

--- SorteioS/settings.py
+++ SorteioS/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-from re import template
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -31,16 +30,25 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-INSTALLED_APPS = [
+DJANGO_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'bootstrap5'
-    'base',
 ]
+
+OTHER_APPS = [
+    'bootstrap5',
+]
+
+MY_APPS = [
+    'base'
+]
+
+INSTALLED_APPS = DJANGO_APPS + OTHER_APPS + MY_APPS
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -108,7 +116,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'pt-br'
 
-TIME_ZONE = 'UTC-3'
+TIME_ZONE = 'America_Sao_Paulo'
 
 USE_I18N = True
 
@@ -123,6 +131,9 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static"
 ]
+
+MEDIA_URL = '/img/'
+MEDIA_ROOT = BASE_DIR/'static/img/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
