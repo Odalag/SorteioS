fb539ba3c3067003ddf754253c8d15cbf2a3c9fe
Maria Luiza Lopes <ramos.luiza@escolar.ifrn.edu.br>
2022-08-31 17:24:07 -0300

    Atualização das páginas e do css. -by Luiza

--- SorteioS/urls.py
+++ SorteioS/urls.py
@@ -21,5 +21,5 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('base/', include('base.urls')),
+    path('home/', include('base.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- static/scripts.js
+++
@@ -1,2 +0,0 @@
-//window.alert("Olá Mundo!");
-console.log("Olá mundo no terminal...");
